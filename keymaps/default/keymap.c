fd6ae2a249181f0da8434c6f29beefd86fa6ba16
Ioannis Maliouris <giannismaliouris@gmail.com>
2025-03-21 13:06:51 +0200

    chore: update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT_split_3x6_5(
         KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,    KC_DLR,   KC_PERC,                  KC_CIRC,    KC_HOME,     KC_UP,       KC_END,  KC_PSCR,  KC_DELETE,
         KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_LEFT,    KC_LEFT,     KC_DOWN,     KC_RIGHT, KC_TRNS,  KC_TRNS,
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_PGDN,     KC_PGUP,     KC_TRNS,  KC_TRNS,  KC_TRNS,
                     KC_TRNS,  KC_TRNS,  MO(_LOWER), KC_TRNS,   KC_TRNS,                 KC_TRNS,    KC_TRNS,     MO(_RAISE),  KC_TRNS,  QK_BOOT
     ),
 
@@ -46,16 +46,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ),
 
     [_LOWER] = LAYOUT_split_3x6_5(
-        KC_GRV,     KC_1,     KC_2,     KC_3,       KC_4,     KC_5,                     KC_6,       KC_7,     KC_8,             KC_9,             KC_0,             KC_MINUS,
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_MINS,    KC_TRNS,  RALT_T(KC_LBRC),  RSFT_T(KC_RBRC),  RCTL_T(KC_BSLS),  KC_EQL,
-        KC_TRNS,    KC_F1,    KC_F2,    KC_F3,      KC_F4,    KC_F5,                    KC_F6,      KC_F7,    KC_F8,            KC_F9,            KC_F10,           KC_F11,
-                    QK_BOOT,  KC_TRNS,  MO(_LOWER), KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  MO(_RAISE),       KC_TRNS,          KC_F12
+        KC_GRV,     KC_1,     KC_2,     KC_3,       KC_4,     KC_5,                     KC_6,       KC_7,                 KC_8,             KC_9,             KC_0,             KC_TRNS,
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_MINS,    RGUI_T(KC_LBRC),      RALT_T(KC_RBRC),  RSFT_T(KC_MINS),  RCTL_T(KC_EQL),  KC_TRNS,
+        KC_TRNS,    KC_F1,    KC_F2,    KC_F3,      KC_F4,    KC_F5,                    KC_F6,      KC_F7,    KC_F8,      KC_F9,            KC_F10,           KC_F11,
+                    QK_BOOT,  KC_TRNS,  MO(_LOWER), KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  MO(_RAISE), KC_TRNS,          KC_F12
     ),
 
     [_ADJUST] = LAYOUT_split_3x6_5(
         KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_7,     KC_8,       KC_9,  KC_TRNS,  KC_TRNS,
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_4,     KC_5,       KC_6,  KC_TRNS,  KC_TRNS,
-        KC_F1,      KC_F2,    KC_F3,    KC_F4,      KC_F5,    KC_F6,                    KC_F7,      KC_1,     KC_2,       KC_3,   KC_F11,   KC_F12,
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_4,     KC_5,       KC_6,  KC_F11,  KC_F12,
+        KC_TRNS,    KC_F1,      KC_F2,    KC_F3,    KC_F4,      KC_F5,                  KC_F7,      KC_F6,    KC_F7,     KC_F8,   KC_F9,   KC_F10,
                     KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                    KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS
     )
 };
